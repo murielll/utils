28a86e4f95cc59aa26f7688db892d348632c9770
Dude <m.abodonow@gmail.com>
2022-10-11 15:13:07 +0300

    Create haproxy_parser.py

---
+++ haproxy_parser.py
@@ -0,0 +1,18 @@
+import re
+'''
+Parse haproxy.cfg and print in format "ip port listen-title"
+'''
+
+f = open('haproxy.cfg')
+for line in f:
+	if 'listen ' in line:
+	    title = re.findall("listen (.+)", line)[0]
+	if 'bind ' in line:
+	  if re.search("bind :", line):
+	  	continue
+	  ip = re.findall("\d+.\d+.\d+.\d+", line)[0]
+	  if ip.startswith("10.50"):
+	    continue
+	  port = re.findall(":(\d+)", line)[0]
+	  print(ip, port, title, sep="\t")
+f.close()
